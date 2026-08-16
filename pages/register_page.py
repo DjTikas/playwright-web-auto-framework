fe9302145646f2ba527ebd97d04ce4427cd56330
Dr_Tikas <1796654305@qq.com>
2026-08-16 22:54:53 +0800

    写了注册页面对象，及其用例

---
+++ pages/register_page.py
@@ -0,0 +1,39 @@
+from playwright.sync_api import Page
+
+class RegisterPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.locator_username = page.get_by_label("用 户 名:")
+        self.locator_password = page.get_by_label("密     码:")
+        self.locator_register_btn = page.get_by_role("button", name="立即注册 > ")
+        self.locator_login_link = page.get_by_text("已有账号？点这登录")
+        # 用户名输入框提示语
+        self.locator_username_tip1 = page.locator('[data-fv-validator="notEmpty"][data-fv-for="username"]')
+        self.locator_username_tip2 = page.locator('[data-fv-validator="stringLength"][data-fv-for="username"]')
+        self.locator_username_tip3 = page.locator('[data-fv-validator="regexp"][data-fv-for="username"]')
+        # 密码输入框提示语
+        self.locator_password_tip1 = page.locator('[data-fv-validator="notEmpty"][data-fv-for="password"]')
+        self.locator_password_tip2 = page.locator('[data-fv-validator="stringLength"][data-fv-for="password"]')
+        self.locator_password_tip3 = page.locator('[data-fv-validator="regexp"][data-fv-for="password"]')
+        # 提示用户已存在
+        self.locator_register_error = page.get_by_text('用户名已存在或不合法！')
+
+    def navigate(self):
+        self.page.goto('/register.html')
+
+    def fill_username(self, username):
+        self.locator_username.fill(username)
+
+    def fill_password(self, password):
+        self.locator_password.fill(password)
+
+    def click_register_btn(self):
+        self.locator_register_btn.click()
+
+    def click_login_link(self):
+        self.locator_login_link.click()
+
+    def register(self, username, password):
+        self.fill_username(username)
+        self.fill_password(password)
+        self.click_register_btn()
