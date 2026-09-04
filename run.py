e65545503e964b2746a3a6a33bb92e79e2fbcac8
Dr_Tikas <1796654305@qq.com>
2026-09-04 20:32:41 +0800

    新增了多账户切换功能，用例里面可以使用管理员账号删除其他账号刚刚新增的项目

--- run.py
+++ run.py
@@ -1,16 +1,13 @@
 import os
 
 import pytest
-#
-# def test_demo(page): # 注意参数是page，插件原生fixture
-#     assert False
 
 
 if __name__ == '__main__':
     # 运行测试用例
-    pytest.main(['./cases/test_project', '--alluredir', './reports'])
+    # pytest.main(['./cases/test_project', '--alluredir', './reports'])
     # pytest.main(['./cases/test_auth', '--alluredir', './reports'])1
-    # pytest.main(['--alluredir', './reports'])
+    pytest.main(['--alluredir', './reports'])
     # 生成测试报告
     os.system('allure generate ./reports -o ./allure_report --clean --lang zh')
     # 打开测试报告
